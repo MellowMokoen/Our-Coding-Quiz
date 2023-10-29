5613729020e12066e5aa8d8e4934d5b687d5b8f7
MellowMokoen <mamello3632@gmail.com>
2023-10-29 23:08:46 +0200

    Defined questions in an array

---
+++ src/quiz.js
@@ -0,0 +1,64 @@
+// Define questions, choices, and correct answers
+let questions = [
+    {
+        question: "Which of the following is not a valid CSS selector?",
+        choices: ["", "", "", ""],
+        correctAnswer: "",
+    },
+    
+    {
+        question: "What is the result of 5 + '5' in JavaScript?",
+        choices: ["", "", "", ""],
+        correctAnswer: "",
+    },
+
+    {
+        question: "In JavaScript, what is the purpose of the 'typeof' operator?",
+        choices: ["", "", "", ""],
+        correctAnswer: "",
+    },
+
+    {
+        question: "Which of the following is not a programming language?",
+        choices: ["", "", "", ""],
+        correctAnswer: "",
+    },
+
+    {
+        question: "What does the CSS property 'display: none;' do?",
+        choices: ["", "", "", ""],
+        correctAnswer: "",
+    },
+
+    {
+        question: "Choose the correct HTML element to define emphasized text?",
+        choices: ["", "", "", ""],
+        correctAnswer: "",
+    },
+
+    {
+        question: "Which of the following is a version control system?",
+        choices: ["", "", "", ""],
+        correctAnswer: "",
+    },
+
+    {
+        question: "Which is used to insert special character in JavaScript?",
+        choices: ["", "", "", ""],
+        correctAnswer: "",
+    },
+
+    {
+        question: "Which is one is a server-side JavaScript object?",
+        choices: ["", "", "", ""],
+        correctAnswer: "",
+    },
+
+    {
+        question: "What is the primary function of a constructor in JavaScript?",
+        choices: ["", "", "", ""],
+        correctAnswer: "",
+    },
+];
+
+
