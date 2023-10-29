1a068b77ae5fa11385198c8e02623f7cd83a9cdf
MellowMokoen <mamello3632@gmail.com>
2023-10-29 23:20:46 +0200

    Defined the correct answers

--- src/quiz.js
+++ src/quiz.js
@@ -3,61 +3,61 @@ let questions = [
     {
         question: "Which of the following is not a valid CSS selector?",
         choices: [".my-class", "#my_id", "$invalid", "h1"],
-        correctAnswer: "",
+        correctAnswer: "$invalid",
     },
     
     {
         question: "What is the result of 5 + '5' in JavaScript?",
         choices: [10,"'55'","5","Error"],
-        correctAnswer: "",
+        correctAnswer: "'55'",
     },
 
     {
         question: "In JavaScript, what is the purpose of the 'typeof' operator?",
         choices: ["To determine the type of a value or variable","To check if a variable is defined","To create new variables","To assign a new type to a variable"],
-        correctAnswer: "",
+        correctAnswer: "To determine the type of a value or variable",
     },
 
     {
         question: "Which of the following is not a programming language?",
         choices: ["Python","Java","C++","HTML"],
-        correctAnswer: "",
+        correctAnswer: "HTML",
     },
 
     {
         question: "What does the CSS property 'display: none;' do?",
         choices: ["Makes an element transparent","Removes an element from the DOM","Hides an element and leaves a blank space","Centers an element on the page"],
-        correctAnswer: "",
+        correctAnswer: "Removes an element from the DOM",
     },
 
     {
         question: "Choose the correct HTML element to define emphasized text?",
         choices: ["italic","i","em","br"],
-        correctAnswer: "",
+        correctAnswer: "em",
     },
 
     {
         question: "Which of the following is a version control system?",
         choices: ["VSCode", "JavaScript", "Git", "Browser"],
-        correctAnswer: "",
+        correctAnswer: "Git",
     },
 
     {
         question: "Which is used to insert special character in JavaScript?",
         choices: ["&", "/","%","-"],
-        correctAnswer: "",
+        correctAnswer: "/",
     },
 
     {
         question: "Which is one is a server-side JavaScript object?",
         choices: ["File", "Function", "Data", "All"],
-        correctAnswer: "",
+        correctAnswer: "File",
     },
 
     {
         question: "What is the primary function of a constructor in JavaScript?",
         choices: [" To remove properties from an object", "To create and initialize objects", "To add methods to an object","To modify the prototype of an object"],
-        correctAnswer: "",
+        correctAnswer: "To create and initialize objects",
     },
 ];
 
